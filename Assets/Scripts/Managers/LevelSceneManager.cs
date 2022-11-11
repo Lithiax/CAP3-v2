5bbd580f5d0f771cfd704778ab3eede552293901
Lithiax <kaell0325@gmail.com>
2022-11-12 03:00:32 +0800

    ensures theres only one event system in FindR

--- Assets/Scripts/Managers/LevelSceneManager.cs
+++ Assets/Scripts/Managers/LevelSceneManager.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class LevelSceneManager : MonoBehaviour
 {
     List<string> activeSceneNames = new List<string>();
     void Awake()
     {
+        //Ensure theres only one pause menu
         for (int i = 0; i > SceneManager.sceneCount; i++)
         {
             activeSceneNames.Add(SceneManager.GetSceneAt(i).name);
@@ -16,6 +18,17 @@ public class LevelSceneManager : MonoBehaviour
         if (activeSceneNames.Contains("PauseMenu"))
         {
             SceneManager.LoadSceneAsync("PauseMenu", LoadSceneMode.Additive);
+        }
+
+        //Ensure theres only one event system
+
+        EventSystem[] es = FindObjectsOfType<EventSystem>();
+        if (es.Length > 1)
+        {
+            for (int i = 1; i < es.Length; i++)
+            {
+                Destroy(es[i].gameObject);
+            }
         }
     }
 
